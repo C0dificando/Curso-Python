8073b5d0d26f7cc42dbfe34268b6060bb728c20d
Codificando <83729419+C0dificando@users.noreply.github.com>
2022-06-05 18:50:07 -0500

    Add files via upload

---
+++ Ejercicios/segundo.py
@@ -0,0 +1,12 @@
+from unicodedata import decimal
+from xmlrpc.client import boolean
+
+
+cadena = "HOla mundo"
+numero = 78
+decimal = 78.56
+boolean3 = False
+
+print(boolean3)
+
+#Declara una varible con tu nombre, tu edad y un boolean
