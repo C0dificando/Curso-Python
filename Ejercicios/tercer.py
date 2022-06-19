ef5fd96de0312279b96081af4eff68c390bd5104
Codificando <83729419+C0dificando@users.noreply.github.com>
2022-06-19 11:56:33 -0500

    Create tercer.py

---
+++ Ejercicios/tercer.py
@@ -0,0 +1,21 @@
+texto = "12345"
+texto2= "1"
+
+flotante = "34.23"
+
+print("1=",type(texto+texto2))
+print(texto+texto2)
+
+texto = int(texto)
+
+print("2=",type(texto))
+
+print(texto+int(texto2))
+
+print(flotante)
+
+print(flotante)
+print(float(flotante))
+print(type(float(flotante)))
+
+#Declarar 3 variables, edad, edad/3 e imprimir tipo de cada variable y van imprimir el tipo de dato -> tipo de dato correcto
