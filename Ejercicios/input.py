f07729bc43397bfb18fb6783c6ac568db6a8acc2
Codificando <83729419+C0dificando@users.noreply.github.com>
2022-07-31 10:45:40 -0500

    Add files via upload
    https://www.youtube.com/watch?v=zqrxfeNeBcA

---
+++ Ejercicios/input.py
@@ -0,0 +1,7 @@
+cadena = int(input("Ingresa un numero "))
+cadena2 = int(input("Ingresa el segundo numero "))
+print("Resultdado",cadena+cadena2)
+
+#Ingresar tres numeros, vas a imprimir como resultado la suma de los 3 numeros y ademas vas a imprimir
+#Castear la suma de los 3 numeros a un string
+#Imprimir el tipo de dato del resultado de los 3 numeros
