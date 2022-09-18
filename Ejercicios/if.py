8a102d0e96f5c1396a125f0de39cd174f20cd628
Codificando <83729419+C0dificando@users.noreply.github.com>
2022-09-18 09:41:04 -0500

    If

---
+++ Ejercicios/if.py
@@ -0,0 +1,12 @@
+pesos  = 50
+
+if pesos > 900:
+    print("Puede entrar en la tienda")
+elif pesos >100:
+    print("Comprar un boleto")
+else:
+    print("Ve a al banco")
+
+print("FInal")
+
+#Pregunte al usuario tres numeros y nos imprima cual es el numero mas alto de los 3
