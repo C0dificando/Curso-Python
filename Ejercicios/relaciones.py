6be81b29065555a5710c7f72deec7e9f2a1620c7
Codificando <83729419+C0dificando@users.noreply.github.com>
2022-12-02 19:12:27 -0600

    https://www.youtube.com/watch?v=KsE8qSn7mAQ

---
+++ Ejercicios/relaciones.py
@@ -0,0 +1,9 @@
+retirar = int(input("Cuanto retiras "))
+
+if(retirar < 100): #-,0,1,2,3,4,5.....,99
+    print("No es multiplo de 100")
+elif(retirar != 9000):
+    print("Sacar en caja")
+else:
+    print("NO paso nada")
+
