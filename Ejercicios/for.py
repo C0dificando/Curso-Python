1fa43db698c37303e35a88e15b9bb80064706822
Codificando <83729419+C0dificando@users.noreply.github.com>
2022-12-02 18:59:36 -0600

    Add files via upload

---
+++ Ejercicios/for.py
@@ -0,0 +1,9 @@
+for i in range(5):
+    if(i==3):
+        print("Es el nummero 3")
+    else:
+        print("No es el numero 3")
+    #i=0
+# 0 1 2 3 4 5 6 7
+
+#Imprimir las tablas de multiplicar del 1 al 10
