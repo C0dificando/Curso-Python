1b636583e6a535bbb4fd16d5356435ffa35acd7b
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-02-25 22:53:39 -0600

    https://www.youtube.com/watch?v=kXMTHCL5I0A

---
+++ Ejercicios/join.py
@@ -0,0 +1,15 @@
+nombres = ["Juan", "Maria", "Jose"]
+lista = ["Pedro", "Ramon"]
+
+print(nombres)
+
+resultado = " ".join([" ".join(nombres)," ".join(lista)]) 
+
+print(resultado)
+
+#Convertir los arreglos
+#["Manzana","Platano","Uvas"]
+#["Juan","Pedro","Rosario"]
+#Unir las cadenas por medio del string "ellos comen"
+
+#Resultado -> JUan Pedro Rosario ellos comen Manzana Platano Uvas
