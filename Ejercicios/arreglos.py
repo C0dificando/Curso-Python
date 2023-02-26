120975c64c965cf38adf8b8f95d84f9ff6d90ca0
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-02-25 22:51:43 -0600

    https://www.youtube.com/watch?v=P7fipvJ52rw

---
+++ Ejercicios/arreglos.py
@@ -0,0 +1,16 @@
+arreglos = ["h","o","l","a"]
+
+print(arreglos)
+
+for x in arreglos:
+    if x == "o":
+        arreglos[1] = "O"
+    if x == "a":
+        arreglos[3] = "A"
+
+
+#Programar la estructura de tu CURP
+
+print(arreglos)
+#h o l a f w t i 
+#0 1 2 3
