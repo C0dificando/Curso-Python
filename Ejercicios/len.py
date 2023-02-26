93f5c94217e959f11f7959bc77063181b7389f79
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-02-25 22:54:35 -0600

    https://www.youtube.com/watch?v=hmwqq54UY9M

---
+++ Ejercicios/len.py
@@ -0,0 +1,13 @@
+fruta = "pera"
+
+longitud = len(fruta)
+
+print("La longitud del string es: "+str(longitud))
+
+#Crea una lista de nombres de coches y almacenala en una variable "coches"
+#Usa la funcion len para calcular el numero de coches en una lista y guadalo
+#en una variable llamada "num_coches"
+#Imprimie el resultado de num_coches en pantalla con la siguiente frase
+#Hay x coches en la lista
+
+#Salida: "Hay 5 coches en la lista"
