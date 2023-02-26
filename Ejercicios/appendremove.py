361b1e1fb346c1d1ccd561fd238c17a819a34b0c
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-02-25 22:45:43 -0600

    Add files via upload
    https://www.youtube.com/watch?v=S9tlWklDseQ

---
+++ Ejercicios/appendremove.py
@@ -0,0 +1,17 @@
+cine = []
+
+for x in range(5):
+    cine.append(input("Agregar pelicula: "+str(x+1)))
+
+print(cine)
+
+eliminar = input("Quitar pelicula")
+
+cine.remove(eliminar)
+
+print(cine)
+
+#Escribir un programa donde ingreses 5 numeros en un arreglo y que se elimine el numero menos
+#del arreglo por medio de un input
+
+#9 5 7 8
