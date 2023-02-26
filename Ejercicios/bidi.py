4980db45c82d814699adbfbac2578039c73cec2b
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-02-25 22:48:06 -0600

    https://www.youtube.com/watch?v=r63y6othHe8

---
+++ Ejercicios/bidi.py
@@ -0,0 +1,21 @@
+bidi = [["1","2","3"],
+        ["4","5","6"],
+        ["7","8","9"]]
+
+        
+
+for x in range(len(bidi)):
+    for j in range(len(bidi[x])):
+        print(bidi[x][j]+" ", end="")
+    print("")
+
+print(bidi)
+
+print()
+
+# 0 0 0
+#0 0 0
+#0 0 0
+
+#Crea dos matrices de 3x3 con numeros enteros y realiza la suma de las 
+# matrices
