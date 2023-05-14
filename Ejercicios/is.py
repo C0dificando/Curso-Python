083c85455036377a68439729fd4347a0282a251e
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-05-14 12:54:56 -0600

    https://www.youtube.com/watch?v=LVI9zw3xCUE

---
+++ Ejercicios/is.py
@@ -0,0 +1,11 @@
+cadena  = "abc"
+if(cadena.isdigit()):
+    print("La cadena ",cadena," contiene unicamente digitos")
+else:
+    print("La cadena ",cadena," no contiene digitos")
+
+#Escriba un programa que solicite al usuario que ingrese una cadena de caracteres y 
+# determine si la cadena ingresada contiene al menos una letra y al menos un número. 
+# Si es así, imprima un mensaje que diga "La cadena ingresada contiene al menos una 
+# letra y al menos un número". De lo contrario, imprima un mensaje que diga 
+# "La cadena ingresada no contiene al menos una letra y al menos un número".
