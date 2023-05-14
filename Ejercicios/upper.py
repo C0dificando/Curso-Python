335720b6a82037ffcfbfd1280ffc2a8189e4495a
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-05-14 12:57:05 -0600

    https://www.youtube.com/watch?v=Dq0czucvyKc

---
+++ Ejercicios/upper.py
@@ -0,0 +1,9 @@
+nombre = "Juan"
+nombreMAy = nombre.upper()
+
+print(nombre)
+print(nombreMAy)
+
+#Comparar dos cadenas donde se utilice la función upper, si la cadena son iguales debe
+#imprimir, en el caso contrario debe imprimir que son diferentes
+#Nota las cadenas deben ser ingresadas por teclado
