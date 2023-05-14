cc386db23f3895ddc53448906aa47f43fdf5e976
Codificando <83729419+C0dificando@users.noreply.github.com>
2023-05-14 12:55:56 -0600

    https://www.youtube.com/watch?v=2ESEi_GdTPU

---
+++ Ejercicios/lower.py
@@ -0,0 +1,15 @@
+cadena = "EStO ES uN eJEMPLO"
+
+cadena = cadena.lower()
+
+#JuaN
+#NO EXISTE USUARIO
+#variable = juan
+
+print(cadena)
+
+#Ejercicio
+#Crea un programa que pida al usuario que introduzca su nombre y apellido. 
+# Luego, utiliza la función lower() para convertir el nombre y el apellido 
+# a minúsculas y concatena ambos valores en una sola cadena separados por un espacio. 
+# Finalmente, imprime la cadena resultante en la pantalla.
